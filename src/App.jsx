3d8db3ee0764af16d61eca18c15d68a7b3d2b57b
dferreiraz <davi2580vege@gmail.com>
2026-08-19 16:22:38 -0300

    fix: corrige case de sectionCards e sectionAcessories

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { Header } from "./components/header/header"
-import { Main } from "./components/main/main"
-import { SectionCards } from "./components/sectionCards/sectionCards"
-import { SectionAcessories } from "./components/sectionAcessories/sectionAcessories"
-import { Contact } from "./components/contact/contact"
-import { Footer } from "./components/footer/footer"
+import { Header } from "./components/header/header.jsx"
+import { Main } from "./components/main/main.jsx"
+import { SectionCards } from "./components/sectionCards/sectionCards.jsx"
+import { SectionAcessories } from "./components/sectionAcessories/sectionAcessories.jsx"
+import { Contact } from "./components/contact/contact.jsx"
+import { Footer } from "./components/footer/footer.jsx"
 import './App.css'
 
 
