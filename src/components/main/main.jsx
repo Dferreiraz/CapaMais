bccabe93dc627958faa4603f76ddc874d15628be
dferreiraz <davi2580vege@gmail.com>
2026-08-20 18:20:55 -0300

    fix: corrected buttons a again

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -12,7 +12,8 @@ export const Main = () => {
                 <p>Consertos rápidos, acessórios de qualidade e atendimento especializado para deixar seu aparelho como novo. Especialistas em <span>Apple, Samsung</span> e as principais marcas.</p>
                 
                 <div className="btn-main">
-                    <a className='btn-primary'>Solicitar orçamento</a>
+                    <a className='btn-primary' href='https://wa.me/5511911219846?text=Ol%C3%A1!%20Gostaria%20de%20solicitar%20um%20orçamento.'
+                        target="_blank" rel="noopener noreferrer">Solicitar orçamento</a>
                     <div className="btn-secondary">
                         <a href='https://wa.me/5511911219846?text=Ol%C3%A1!%20Gostaria%20de%20mais%20informa%C3%A7%C3%B5es.'
                         target="_blank" rel="noopener noreferrer">Falar no whatsApp</a>
